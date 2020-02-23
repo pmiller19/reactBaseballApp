babf0f3c4b3b5e21548da75227c7dab9e85cf2ca
Parker Miller <pmiller19@huskers.unl.edu>
2020-02-22 18:58:07 -0600

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Header.css';
+import Logo from "/Users/parker/Documents/GitHub/reactBaseballApp/src/unlLogo2.jpg";
 
 function Header() {
 
     return (
-        <div className="App">
-            <h1>Header</h1>
+        // <div className="App">
+        //     <h1>Header</h1>
+        // </div>
+        <div className="heading">
+            <a href="../index.html">
+                <img className="heading-child" src={Logo} width="55" height="55" alt="logo" />
+
+                
+                <p className="heading-text"> Baseball </p>
+            </a>
         </div>
     );
 }
